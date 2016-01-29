a3b591604402865f13016e69f070b3ed7f7e226c
Marek Kurdej <marek@quasardb.net>
2016-01-29 10:07:58 +0100

    Check for php in PATH when generating documentation.

--- scripts/generate-doc.bat
+++ scripts/generate-doc.bat
@@ -4,6 +4,10 @@ set TITLE=quasardb
 set INPUT_DIR=%~dp0..\doc
 set OUTPUT_DIR=%~dp0..\doc-out
 set PHP=C:\Program Files (x86)\php-5.6.10-nts-Win32-VC11-x86\php.exe
+echo %PHP%
+for /f "delims=" %%p in ('where php') do (
+    set PHP=%%p
+)
 set APIGEN=%~dp0..\third-party\apigen.phar
 set TEMPLATE_CONFIG=%~dp0..\doc-theme\config.neon
 
