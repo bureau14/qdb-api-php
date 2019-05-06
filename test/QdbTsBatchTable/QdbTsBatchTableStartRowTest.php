9b432f8a8fc372ba8b8caf39dcd4f230cd0ce4b4
Sidney Congard <sidney@quasardb.net>
2019-05-06 15:03:11 +0200

    corrected scanned_point_count

--- test/QdbTsBatchTable/QdbTsBatchTableStartRowTest.php
+++ test/QdbTsBatchTable/QdbTsBatchTableStartRowTest.php
@@ -22,6 +22,27 @@ class QdbTsBatchTableStartRowTest extends QdbTestBase
         $batch->set_blob(0, 'Bob');
         $batch->set_int64(1, 22);
         $batch->push_values();
+
+        $query = $this->cluster->makeQuery('SELECT * FROM persons');
+        $this->assertEquals(count($query->tables()), 1);
+        $this->assertEquals($query->scannedPointCount(), 6);
+
+        $table = $query->tables()[0];
+        $this->assertEquals($table->table_name(), 'persons');
+        $this->assertEquals($table->columns_names(), ['$timestamp', 'Alice', 'Bob']);
+        $this->assertEquals($table->rows_count(), 2);
+        $this->assertEquals($table->get_point(0, 0).type(), QdbQueryPoint::TIMESTAMP);
+        $this->assertEquals($table->get_point(1, 0).type(), QdbQueryPoint::TIMESTAMP);
+        $this->assertEquals($table->get_point(0, 1).type(), QdbQueryPoint::BLOB);
+        $this->assertEquals($table->get_point(1, 1).type(), QdbQueryPoint::BLOB);
+        $this->assertEquals($table->get_point(0, 2).type(), QdbQueryPoint::INT64);
+        $this->assertEquals($table->get_point(1, 2).type(), QdbQueryPoint::INT64);
+        $this->assertEquals($table->get_point(0, 0).value(), new QdbTimestamp(0, 0));
+        $this->assertEquals($table->get_point(1, 0).value(), new QdbTimestamp(1, 0));
+        $this->assertEquals($table->get_point(0, 1).value(), 'Alice');
+        $this->assertEquals($table->get_point(1, 1).value(), 'Bob');
+        $this->assertEquals($table->get_point(0, 2).value(), 21);
+        $this->assertEquals($table->get_point(1, 2).value(), 22);
     }
 }
 
