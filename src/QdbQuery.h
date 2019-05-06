9c5836762b0ba1ecc6f0431ca79bb9cc7bd7d54b
Sidney Congard <sidney@quasardb.net>
2019-05-06 11:19:02 +0200

    renamed other symbols

--- src/QdbQuery.h
+++ src/QdbQuery.h
@@ -1,14 +1,14 @@
 // Copyright (c) 2009-2016, quasardb SAS
 // All rights reserved.
 
-#ifndef QDB_TS_QUERY_H
-#define QDB_TS_QUERY_H
+#ifndef QDB_QUERY_H
+#define QDB_QUERY_H
 
 #include <zend.h>  // include first to avoid conflict with stdint.h
 #include <qdb/query.h>
 
-void QdbTsQuery_registerClass(TSRMLS_D);
+void QdbQuery_registerClass(TSRMLS_D);
 
-void QdbTsQuery_createInstance(zval* destination, qdb_handle_t handle, const char* query TSRMLS_DC);
+void QdbQuery_createInstance(zval* destination, qdb_handle_t handle, const char* query TSRMLS_DC);
 
-#endif /* QDB_TS_QUERY_H */
+#endif /* QDB_QUERY_H */
