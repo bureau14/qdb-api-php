d732ee38fb99815c69471939b37782e149975130
Sidney Congard <sidney@quasardb.net>
2019-10-15 09:57:31 +0200

    updated QdbQuery object and it's tests

--- src/QdbQuery.c
+++ src/QdbQuery.c
@@ -11,7 +11,8 @@ typedef struct
 {
     qdb_handle_t handle;
     qdb_query_result_t* result;
-    zval tables;
+    zval column_names;
+    zval rows;
     zval scanned_point_count;
 } _zval_query_t;
 
@@ -26,7 +27,11 @@ void QdbQuery_createInstance(zval* destination,
 {
     qdb_query_result_t* result = NULL;
     qdb_error_t err = qdb_query(handle, query, &result);
-    if (QDB_FAILURE(err)) throw_qdb_error(err);
+
+    if (QDB_FAILURE(err)) {
+        qdb_release(handle, result);
+        throw_qdb_error(err);
+    }
 
     object_init_ex(destination, ce_QdbQuery);
     class_storage* this = get_class_storage(destination);
@@ -34,21 +39,38 @@ void QdbQuery_createInstance(zval* destination,
     this->result = result;
 
     if (result == NULL) {
+        array_init_size(&this->column_names, 0);
+        array_init_size(&this->rows, 0);
         ZVAL_LONG(&this->scanned_point_count, 0);
-        array_init_size(&this->tables, 0);
         return;
     }
 
-	ZVAL_LONG(&this->scanned_point_count, this->result->scanned_point_count);
-    array_init_size(&this->tables, result->tables_count);
-
-    HashTable* tables_ht = Z_ARR(this->tables);
-	for (int i = 0; i < result->tables_count; i++)
+    // Colum names.
+    array_init_size(&this->column_names, result->column_count);
+	for (int i = 0; i < result->column_count; ++i)
     {
-        zval table;
-        QdbQueryTable_createInstance(&table, &result->tables[i]);
-		zend_hash_next_index_insert(tables_ht, &table);
+        zval name;
+        ZVAL_STRINGL(&name, result->column_names[i].data, result->column_names[i].length);
+		zend_hash_next_index_insert(Z_ARR(this->column_names), &name);
     }
+
+    // Rows.
+    array_init_size(&this->rows, result->row_count);
+	for (int i = 0; i < result->row_count; ++i)
+    {
+        zval row;
+        array_init_size(&row, result->column_count);
+
+        for (int j = 0; j < result->column_count; ++j)
+        {
+            zval point;
+            QdbQueryPoint_createInstance(&point, &result->rows[i][j]);
+            zend_hash_next_index_insert(Z_ARR(row), &point);
+        }
+        zend_hash_next_index_insert(Z_ARR(this->rows), &row);
+    }
+    
+	ZVAL_LONG(&this->scanned_point_count, result->scanned_point_count);
 }
 
 CLASS_METHOD_0(__destruct)
@@ -56,9 +78,14 @@ CLASS_METHOD_0(__destruct)
     qdb_release(this->handle, this->result);
 }
 
-CLASS_METHOD_0(tables)
+CLASS_METHOD_0(columnNames)
 {
-    ZVAL_COPY(return_value, &this->tables);
+    ZVAL_COPY(return_value, &this->columns_names);
+}
+
+CLASS_METHOD_0(rows)
+{
+    ZVAL_COPY(return_value, &this->rows);
 }
 
 CLASS_METHOD_0(scannedPointCount)
@@ -68,7 +95,8 @@ CLASS_METHOD_0(scannedPointCount)
 
 BEGIN_CLASS_MEMBERS()
     ADD_DESTRUCTOR(__destruct)
-    ADD_METHOD(tables)
+    ADD_METHOD(columnNames)
+    ADD_METHOD(rows)
     ADD_METHOD(scannedPointCount)
 END_CLASS_MEMBERS()
 
--- test/QdbQuery/QdbQueryEffectsTest.php
+++ test/QdbQuery/QdbQueryEffectsTest.php
@@ -9,33 +9,40 @@ class QdbQueryEffectsTest extends QdbTestBase
     {
         try {
             $query = $this->cluster->makeQuery('CREATE TABLE persons(name BLOB, age INT64)');
-            $this->assertEquals(count($query->tables()), 0);
+            $this->assertEquals(count($query->columnNames()), 0);
+            $this->assertEquals(count($query->rows()), 0);
             $this->assertEquals($query->scannedPointCount(), 0);
 
             $query = $this->cluster->makeQuery('INSERT INTO persons($timestamp, name, age)'.
                                             'VALUES (1970, "Alice", 21), (1970-01-01T00:00:01, "Bob", 22)');
-            $this->assertEquals(count($query->tables()), 0);
+            $this->assertEquals(count($query->columnNames()), 0);
+            $this->assertEquals(count($query->rows()), 0);
             $this->assertEquals($query->scannedPointCount(), 0);
             
             $query = $this->cluster->makeQuery('SELECT * FROM persons');
-            $this->assertEquals(count($query->tables()), 1);
+            $this->assertEquals(count($query->columnNames()), 4);
+            $this->assertEquals(count($query->rows()), 2);
             $this->assertEquals($query->scannedPointCount(), 4);
 
-            $table = $query->tables()[0];
-            $this->assertEquals($table->tableName(),    'persons');
-            $this->assertEquals($table->columnsNames(), ['$timestamp', 'name', 'age']);
-            $this->assertEquals($table->pointsRows()[0][0]->type(), QdbQueryPoint::TIMESTAMP);
-            $this->assertEquals($table->pointsRows()[1][0]->type(), QdbQueryPoint::TIMESTAMP);
-            $this->assertEquals($table->pointsRows()[0][1]->type(), QdbQueryPoint::BLOB);
-            $this->assertEquals($table->pointsRows()[1][1]->type(), QdbQueryPoint::BLOB);
-            $this->assertEquals($table->pointsRows()[0][2]->type(), QdbQueryPoint::INT64);
-            $this->assertEquals($table->pointsRows()[1][2]->type(), QdbQueryPoint::INT64);
-            $this->assertEquals($table->pointsRows()[0][0]->value(), new QdbTimestamp(0, 0));
-            $this->assertEquals($table->pointsRows()[1][0]->value(), new QdbTimestamp(1, 0));
-            $this->assertEquals($table->pointsRows()[0][1]->value(), 'Alice');
-            $this->assertEquals($table->pointsRows()[1][1]->value(), 'Bob');
-            $this->assertEquals($table->pointsRows()[0][2]->value(), 21);
-            $this->assertEquals($table->pointsRows()[1][2]->value(), 22);
+            $this->assertEquals($query->columnNames(), ['$timestamp', '$table', 'name', 'age']);
+
+            $this->assertEquals($query->pointsRows()[0][0]->type(), QdbQueryPoint::TIMESTAMP);
+            $this->assertEquals($query->pointsRows()[1][0]->type(), QdbQueryPoint::TIMESTAMP);
+            $this->assertEquals($query->pointsRows()[0][1]->type(), QdbQueryPoint::BLOB);
+            $this->assertEquals($query->pointsRows()[1][1]->type(), QdbQueryPoint::BLOB);
+            $this->assertEquals($query->pointsRows()[0][2]->type(), QdbQueryPoint::BLOB);
+            $this->assertEquals($query->pointsRows()[1][2]->type(), QdbQueryPoint::BLOB);
+            $this->assertEquals($query->pointsRows()[0][3]->type(), QdbQueryPoint::INT64);
+            $this->assertEquals($query->pointsRows()[1][3]->type(), QdbQueryPoint::INT64);
+
+            $this->assertEquals($query->pointsRows()[0][0]->value(), new QdbTimestamp(0, 0));
+            $this->assertEquals($query->pointsRows()[1][0]->value(), new QdbTimestamp(1, 0));
+            $this->assertEquals($query->pointsRows()[0][1]->value(), 'persons');
+            $this->assertEquals($query->pointsRows()[1][1]->value(), 'persons');
+            $this->assertEquals($query->pointsRows()[0][2]->value(), 'Alice');
+            $this->assertEquals($query->pointsRows()[1][2]->value(), 'Bob');
+            $this->assertEquals($query->pointsRows()[0][3]->value(), 21);
+            $this->assertEquals($query->pointsRows()[1][3]->value(), 22);
         }
         finally {
             $query = $this->cluster->makeQuery('DROP TABLE persons');
