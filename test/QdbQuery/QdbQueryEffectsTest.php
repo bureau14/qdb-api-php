dd2e236ddb13743dcadab0650e669866d6ae9e72
Sidney Congard <sidney@quasardb.net>
2019-11-08 10:44:21 +0100

    correct test

--- test/QdbQuery/QdbQueryEffectsTest.php
+++ test/QdbQuery/QdbQueryEffectsTest.php
@@ -29,21 +29,21 @@ class QdbQueryEffectsTest extends QdbTestBase
             $this->assertEquals($query->columnNames(), ['$timestamp', '$table', 'name', 'age']);
 
             $this->assertEquals($query->rows()[0][0]->type(), QdbQueryPoint::TIMESTAMP);
-            $this->assertEquals($query->rows()[1][0]->type(), QdbQueryPoint::TIMESTAMP);
             $this->assertEquals($query->rows()[0][1]->type(), QdbQueryPoint::BLOB);
-            $this->assertEquals($query->rows()[1][1]->type(), QdbQueryPoint::BLOB);
             $this->assertEquals($query->rows()[0][2]->type(), QdbQueryPoint::BLOB);
-            $this->assertEquals($query->rows()[1][2]->type(), QdbQueryPoint::BLOB);
             $this->assertEquals($query->rows()[0][3]->type(), QdbQueryPoint::INT64);
-            $this->assertEquals($query->rows()[1][3]->type(), QdbQueryPoint::INT64);
-
             $this->assertEquals($query->rows()[0][0]->value(), new QdbTimestamp(0, 0));
-            $this->assertEquals($query->rows()[1][0]->value(), new QdbTimestamp(1, 0));
             $this->assertEquals($query->rows()[0][1]->value(), 'persons');
-            $this->assertEquals($query->rows()[1][1]->value(), 'persons');
             $this->assertEquals($query->rows()[0][2]->value(), 'Alice');
-            $this->assertEquals($query->rows()[1][2]->value(), 'Bob');
             $this->assertEquals($query->rows()[0][3]->value(), 21);
+
+            $this->assertEquals($query->rows()[1][0]->type(), QdbQueryPoint::TIMESTAMP);
+            $this->assertEquals($query->rows()[1][1]->type(), QdbQueryPoint::BLOB);
+            $this->assertEquals($query->rows()[1][2]->type(), QdbQueryPoint::BLOB);
+            $this->assertEquals($query->rows()[1][3]->type(), QdbQueryPoint::INT64);
+            $this->assertEquals($query->rows()[1][0]->value(), new QdbTimestamp(1, 0));
+            $this->assertEquals($query->rows()[1][1]->value(), 'persons');
+            $this->assertEquals($query->rows()[1][2]->value(), 'Bob');
             $this->assertEquals($query->rows()[1][3]->value(), 22);
         }
         finally {
@@ -63,37 +63,50 @@ class QdbQueryEffectsTest extends QdbTestBase
             $query = $this->cluster->makeQuery('SELECT count(int64), blob, int64, ts, f64 FROM flavours');
 
             $this->assertEquals($query->columnNames(), ['count(int64)', 'blob', 'int64', 'ts', 'f64']);
+            $this->assertEquals(count($query->rows()), 4);
 
             $this->assertEquals($query->rows()[0][0]->type(), QdbQueryPoint::COUNT);
-            $this->assertEquals($query->rows()[1][0]->type(), QdbQueryPoint::NONE);
-            $this->assertEquals($query->rows()[2][0]->type(), QdbQueryPoint::NONE);
             $this->assertEquals($query->rows()[0][1]->type(), QdbQueryPoint::NONE);
-            $this->assertEquals($query->rows()[1][1]->type(), QdbQueryPoint::BLOB);
-            $this->assertEquals($query->rows()[2][1]->type(), QdbQueryPoint::BLOB);
             $this->assertEquals($query->rows()[0][2]->type(), QdbQueryPoint::NONE);
-            $this->assertEquals($query->rows()[1][2]->type(), QdbQueryPoint::NONE);
-            $this->assertEquals($query->rows()[2][2]->type(), QdbQueryPoint::INT64);
             $this->assertEquals($query->rows()[0][3]->type(), QdbQueryPoint::NONE);
-            $this->assertEquals($query->rows()[1][3]->type(), QdbQueryPoint::NONE);
-            $this->assertEquals($query->rows()[2][3]->type(), QdbQueryPoint::TIMESTAMP);
             $this->assertEquals($query->rows()[0][4]->type(), QdbQueryPoint::NONE);
-            $this->assertEquals($query->rows()[1][4]->type(), QdbQueryPoint::NONE);
-            $this->assertEquals($query->rows()[2][4]->type(), QdbQueryPoint::NONE);
-
             $this->assertEquals($query->rows()[0][0]->value(), 2);
-            $this->assertEquals($query->rows()[1][0]->value(), null);
-            $this->assertEquals($query->rows()[2][0]->value(), null);
-            $this->assertEquals($query->rows()[0][1]->value(), 'Alice');
-            $this->assertEquals($query->rows()[1][1]->value(), 'Bob');
-            $this->assertEquals($query->rows()[2][1]->value(), null);
+            $this->assertEquals($query->rows()[0][1]->value(), null);
             $this->assertEquals($query->rows()[0][2]->value(), null);
-            $this->assertEquals($query->rows()[1][2]->value(), 22);
-            $this->assertEquals($query->rows()[2][2]->value(), 23);
             $this->assertEquals($query->rows()[0][3]->value(), null);
-            $this->assertEquals($query->rows()[1][3]->value(), new QdbTimestamp(0, 0));
-            $this->assertEquals($query->rows()[2][3]->value(), null);
             $this->assertEquals($query->rows()[0][4]->value(), null);
+
+            $this->assertEquals($query->rows()[1][0]->type(), QdbQueryPoint::NONE);
+            $this->assertEquals($query->rows()[1][1]->type(), QdbQueryPoint::BLOB);
+            $this->assertEquals($query->rows()[1][2]->type(), QdbQueryPoint::NONE);
+            $this->assertEquals($query->rows()[1][3]->type(), QdbQueryPoint::NONE);
+            $this->assertEquals($query->rows()[1][4]->type(), QdbQueryPoint::NONE);
+            $this->assertEquals($query->rows()[1][0]->value(), null);
+            $this->assertEquals($query->rows()[1][1]->value(), 'Alice');
+            $this->assertEquals($query->rows()[1][2]->value(), null);
+            $this->assertEquals($query->rows()[1][3]->value(), null);
             $this->assertEquals($query->rows()[1][4]->value(), null);
+
+            $this->assertEquals($query->rows()[1][0]->type(), QdbQueryPoint::NONE);
+            $this->assertEquals($query->rows()[1][1]->type(), QdbQueryPoint::BLOB);
+            $this->assertEquals($query->rows()[1][2]->type(), QdbQueryPoint::INT64);
+            $this->assertEquals($query->rows()[1][3]->type(), QdbQueryPoint::TIMESTAMP);
+            $this->assertEquals($query->rows()[1][4]->type(), QdbQueryPoint::NONE);
+            $this->assertEquals($query->rows()[1][0]->value(), null);
+            $this->assertEquals($query->rows()[1][1]->value(), 'Bob');
+            $this->assertEquals($query->rows()[1][2]->value(), 22);
+            $this->assertEquals($query->rows()[1][3]->value(), new QdbTimestamp(0, 0));
+            $this->assertEquals($query->rows()[1][4]->value(), null);
+
+            $this->assertEquals($query->rows()[2][0]->type(), QdbQueryPoint::NONE);
+            $this->assertEquals($query->rows()[2][1]->type(), QdbQueryPoint::NONE);
+            $this->assertEquals($query->rows()[2][2]->type(), QdbQueryPoint::INT64);
+            $this->assertEquals($query->rows()[2][3]->type(), QdbQueryPoint::NONE);
+            $this->assertEquals($query->rows()[2][4]->type(), QdbQueryPoint::NONE);
+            $this->assertEquals($query->rows()[2][0]->value(), null);
+            $this->assertEquals($query->rows()[2][1]->value(), null);
+            $this->assertEquals($query->rows()[2][2]->value(), 23);
+            $this->assertEquals($query->rows()[2][3]->value(), null);
             $this->assertEquals($query->rows()[2][4]->value(), null);
         }
         finally {
