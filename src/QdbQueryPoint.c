6444b313035a8c717efd50490ada6ceb8ce921fa
Sidney Congard <sidney.congard@gmail.com>
2019-11-08 11:58:01 +0100

    QDB-2816 - Support NULL values (#4)
    NULL values are supported from qdb queries.

--- src/QdbQueryPoint.c
+++ src/QdbQueryPoint.c
@@ -5,6 +5,7 @@
 #include "QdbTimestamp.h"
 #include "class_definition.h"
 #include "exceptions.h"
+#include <qdb/ts.h>
 
 typedef struct
 {
@@ -18,7 +19,8 @@ typedef struct
 extern zend_class_entry* ce_QdbQueryPoint;
 
 int init_query_point_types() {
-    return zend_declare_class_constant_long(ce_QdbQueryPoint, "BLOB", sizeof("BLOB")-1, qdb_query_result_blob) == SUCCESS
+    return zend_declare_class_constant_long(ce_QdbQueryPoint, "NONE", sizeof("NONE")-1, qdb_query_result_none) == SUCCESS
+        && zend_declare_class_constant_long(ce_QdbQueryPoint, "BLOB", sizeof("BLOB")-1, qdb_query_result_blob) == SUCCESS
         && zend_declare_class_constant_long(ce_QdbQueryPoint, "COUNT", sizeof("COUNT")-1, qdb_query_result_count) == SUCCESS
         && zend_declare_class_constant_long(ce_QdbQueryPoint, "DOUBLE", sizeof("DOUBLE")-1, qdb_query_result_double) == SUCCESS
         && zend_declare_class_constant_long(ce_QdbQueryPoint, "INT64", sizeof("INT64")-1, qdb_query_result_int64) == SUCCESS
@@ -27,34 +29,39 @@ int init_query_point_types() {
 
 void QdbQueryPoint_createInstance(zval* destination, qdb_point_result_t* point)
 {
-    if (point->type < qdb_query_result_double || point->type > qdb_query_result_count)
-        throw_invalid_argument("Got invalid query point");
-
     object_init_ex(destination, ce_QdbQueryPoint);
     class_storage* this = get_class_storage(destination);
 
     ZVAL_LONG(&this->type, point->type);
-
     switch (point->type)
     {
+    case qdb_query_result_none: break;
     case qdb_query_result_blob:
+        if (QDB_IS_NULL_BLOB(point->payload.blob)) break;
         ZVAL_STRINGL(&this->value, point->payload.blob.content, point->payload.blob.content_length);
         return;
     case qdb_query_result_double:
+        if (QDB_IS_NULL_DOUBLE(point->payload.double_)) break;
         ZVAL_DOUBLE(&this->value, point->payload.double_.value);
         return;
     case qdb_query_result_int64:
+        if (QDB_IS_NULL_INT64(point->payload.int64_)) break;
         ZVAL_LONG(&this->value, point->payload.int64_.value);
         return;
     case qdb_query_result_count:
+        if (point->payload.int64_.value == qdb_count_undefined) break;
         ZVAL_LONG(&this->value, point->payload.count.value);
         return;
-    case qdb_query_result_timestamp:
+    case qdb_query_result_timestamp: {
+        if (QDB_IS_NULL_TIMESTAMP(point->payload.timestamp)) break;
         this->value = QdbTimestamp_from_timespec(&point->payload.timestamp.value);
         return;
+    }
     default:
         throw_out_of_bounds("Wrong point type received from QuasarDB");
     }
+
+    ZVAL_NULL(&this->value);
 }
 
 CLASS_METHOD_0(type)
