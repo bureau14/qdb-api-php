93703d59e7d3e2d790e46f55e87aa3d3d9aa7b71
Sidney Congard <sidney.congard@gmail.com>
2020-10-29 11:02:49 +0100

    added batch inserter sym test

--- test/QdbTsBatchTable/QdbTsBatchTablePushValuesTest.php
+++ test/QdbTsBatchTable/QdbTsBatchTablePushValuesTest.php
@@ -8,21 +8,24 @@ class QdbTsBatchTablePushValuesTest extends QdbTestBase
     public function testFillTable()
     {
         try {
-            $query = $this->cluster->makeQuery('CREATE TABLE persons(name BLOB, age INT64)');
+            $query = $this->cluster->makeQuery('CREATE TABLE persons(name BLOB, age INT64, class SYMBOL)');
             $this->assertEquals(count($query->columnNames()), 0);
             $this->assertEquals(count($query->rows()), 0);
             $this->assertEquals($query->scannedPointCount(), 0);
 
             $batch = $this->cluster->makeBatchTable([
                 new QdbTsBatchColumnInfo('persons', 'name'),
-                new QdbTsBatchColumnInfo('persons', 'age')
+                new QdbTsBatchColumnInfo('persons', 'age'),
+                new QdbTsBatchColumnInfo('persons', 'class')
             ]);
             $batch->startRow(new QdbTimestamp(0, 0));
             $batch->setBlob(0, 'Alice');
             $batch->setInt64(1, 21);
+            $batch->setBlob(2, 'Priest');
             $batch->startRow(new QdbTimestamp(1, 0));
             $batch->setBlob(0, 'Bob');
             $batch->setInt64(1, 22);
+            $batch->setBlob(2, 'Mage');
             $batch->pushValues();
 
             $query = $this->cluster->makeQuery('SELECT * FROM persons');
@@ -30,7 +33,7 @@ class QdbTsBatchTablePushValuesTest extends QdbTestBase
             $this->assertEquals(count($query->rows()), 2);
             $this->assertEquals($query->scannedPointCount(), 4);
 
-            $this->assertEquals($query->columnNames(), ['$timestamp', '$table', 'name', 'age']);
+            $this->assertEquals($query->columnNames(), ['$timestamp', '$table', 'name', 'age', 'class']);
             $this->assertEquals($query->rows()[0][0]->type(), QdbQueryPoint::TIMESTAMP);
             $this->assertEquals($query->rows()[1][0]->type(), QdbQueryPoint::TIMESTAMP);
             $this->assertEquals($query->rows()[0][1]->type(), QdbQueryPoint::STRING);
@@ -39,6 +42,8 @@ class QdbTsBatchTablePushValuesTest extends QdbTestBase
             $this->assertEquals($query->rows()[1][2]->type(), QdbQueryPoint::BLOB);
             $this->assertEquals($query->rows()[0][3]->type(), QdbQueryPoint::INT64);
             $this->assertEquals($query->rows()[1][3]->type(), QdbQueryPoint::INT64);
+            $this->assertEquals($query->rows()[0][4]->type(), QdbQueryPoint::SYBOL);
+            $this->assertEquals($query->rows()[1][4]->type(), QdbQueryPoint::SYBOL);
             $this->assertEquals($query->rows()[0][0]->value(), new QdbTimestamp(0, 0));
             $this->assertEquals($query->rows()[1][0]->value(), new QdbTimestamp(1, 0));
             $this->assertEquals($query->rows()[0][1]->value(), 'persons');
@@ -47,6 +52,8 @@ class QdbTsBatchTablePushValuesTest extends QdbTestBase
             $this->assertEquals($query->rows()[1][2]->value(), 'Bob');
             $this->assertEquals($query->rows()[0][3]->value(), 21);
             $this->assertEquals($query->rows()[1][3]->value(), 22);
+            $this->assertEquals($query->rows()[0][4]->value(), 'Priest');
+            $this->assertEquals($query->rows()[1][4]->value(), 'Mage');
         }
         finally {
             $query = $this->cluster->makeQuery('DROP TABLE persons');
