313760f699f182ffd165a90f9420a2dc644fc119
Marek Kurdej <marek@quasardb.net>
2021-05-21 12:25:12 +0200

    QDB-9674 - Remove successful operation in last error message.

--- test/QdbCluster/QdbClusterLastErrorTest.php
+++ test/QdbCluster/QdbClusterLastErrorTest.php
@@ -11,7 +11,7 @@ class QdbClusterLastErrorTest extends QdbTestBase
             $this->assertEquals('Should not reach this assert'. 0);
         } catch (Exception $e) {}
         $msg1 = $this->cluster->lastError();
-        
+
         try {
             $integer = $this->createEmptyInteger();
             $integer->put(1);
@@ -19,13 +19,9 @@ class QdbClusterLastErrorTest extends QdbTestBase
             $this->assertEquals('Should not reach this assert'. 0);
         } catch (Exception $e) {}
         $msg2 = $this->cluster->lastError();
-        
-        $this->cluster->makeQuery('CREATE TABLE t_a_b_l_e(col INT64)');
-        $msg3 = $this->cluster->lastError();
-        
+
         $this->assertEquals($msg1, 'at qdb_remove: An entry matching the provided alias cannot be found.');
         $this->assertEquals($msg2, 'at qdb_int_put: An entry matching the provided alias already exists.');
-        $this->assertEquals($msg3, 'at qdb_query: The operation completed successfully.');
     }
 }
 
